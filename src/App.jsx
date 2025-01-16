83f6315a6115e2130dcbd5ec050648ebf468c30f
Anchal Yadav <anchal.code@gmail.com>
2025-01-16 23:06:58 +0530

    button color change

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ function App() {
               <input
                 type="submit"
                 value="Search"
-                className="form-control btn btn-danger"
+                className="form-control btn btn-info"
               />
             </form>
           </div>
